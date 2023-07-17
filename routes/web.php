685653e144fc926a53e150712e4c8e40a0b1d9f8
chalaa <chalaolanigeleta@gmail.com>
2023-07-17 17:59:18 +0300

    update the route

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,9 @@
 
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomePageController;
-use App\Models\House;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HouseController;
-use App\Models\HomePage;
-use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,44 +19,33 @@ use Symfony\Component\HttpFoundation\Response;
 //all house 
 Route::get('/', [HomePageController::class, 'index']);
 
-//single house
 Route::get('/houses/{house}', [HouseController::class, 'show']);
-
-//show admin page
-Route::get('/admin', [HouseController::class, 'adminPage'])->middleware('auth');
-
-//create new house
-Route::get('/admin/houses/create', [HouseController::class, 'create'])->middleware('auth');
-
-
-//add new house to database
-Route::post('/admin/houses', [HouseController::class, 'store'])->middleware('auth');
-
-//get all house on admin page
-Route::get('/admin/show', [HouseController::class, 'show_house_admin'])->middleware('auth');
-
-// Show Edit Form
-Route::get('/admin/houses/{house}/edit', [HouseController::class, 'edit'])->middleware('auth');
-
-//Update house 
-Route::put('/admin/houses/{house}', [HouseController::class, 'update'])->middleware('auth');
-
-//delete house 
-Route::delete('/admin/houses/{house}', [HouseController::class, 'destroy'])->middleware('auth');
-//for house
 Route::get('/houses', [HouseController::class, 'index']);
-
-//for car
-//all cars 
 Route::get('/cars', [CarController::class, 'index']);
 Route::get('/cars/{car}', [CarController::class, 'show']);
-Route::get('/admin/cars/create', [CarController::class, 'create'])->middleware('auth');
-Route::post('/cars/store', [CarController::class, 'store']);
-Route::get('/admin/cars/show', [CarController::class, 'show_car_admin'])->middleware('auth');
-Route::get('/admin/cars/{car}/edit', [CarController::class, 'edit'])->middleware('auth');
-Route::put('/admin/cars/{car}', [CarController::class, 'update'])->middleware('auth');
-Route::delete('/admin/cars/{car}', [CarController::class, 'destroy'])->middleware('auth');
 
+Route::middleware(['auth'])->group(function(){
+    
+    //admin routes
+    Route::get('/admin', [HouseController::class, 'adminPage'])->name("admin_home");
+
+    //admin house routes
+    Route::get('/admin/houses/create', [HouseController::class, 'create'])->name("admin_create_house");
+    Route::post('/admin/houses', [HouseController::class, 'store'])->name("admin_store_house");
+    Route::get('/admin/show', [HouseController::class, 'show_house_admin'])->name("admin_show_house");
+    Route::get('/admin/houses/{house}/edit', [HouseController::class, 'edit'])->name("admin_edit_house");
+    Route::put('/admin/houses/{house}', [HouseController::class, 'update'])->name("admin_update_house");
+    Route::delete('/admin/houses/{house}', [HouseController::class, 'destroy'])->name("admin_delete_house");
+
+    // admin car routes
+    Route::get('/admin/cars/create', [CarController::class, 'create'])->name("admin_create_car");
+    Route::post('/cars/store', [CarController::class, 'store'])->name("admin_store_car");
+    Route::get('/admin/cars/show', [CarController::class, 'show_car_admin'])->name("admin_show_car");
+    Route::get('/admin/cars/{car}/edit', [CarController::class, 'edit'])->name("admin_edit_car");
+    Route::put('/admin/cars/{car}', [CarController::class, 'update'])->name("admin_update_car");
+    Route::delete('/admin/cars/{car}', [CarController::class, 'destroy'])->name("admin_delete_car");
+
+});
 //show registration form
 Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
@@ -75,35 +60,3 @@ Route::get('/login', [UserController::class, 'login'])->name('login')->middlewar
 
 // Log In User
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
-
-
-
-
-//admin test
-// Route::get('/admin', function () {
-//     return view('admin.index');
-// })->middleware('auth');
-
-// //add home
-// Route::get('/admin/add', function(){
-//     return view('admin.add-house');
-// });
-
-
-
-// Route::get('/hello', function () {
-//     return response('<h1>hello world!</h1>');
-// });
-// Route::get('/post/{id}', function($id)
-// {
-//     dd($id);
-//     return response('Post '.$id);
-// })->where('id', '[0_9]+');
-
-
-// Route::get('/search', function(Request $request)
-
-// {
-//     dd($request);
-//     // return response($request);
-// });
