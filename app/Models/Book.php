5abfdaf9c5ff450220ee786d32346bbc2bba66fe
chalaa <chalaolanigeleta@gmail.com>
2023-07-19 18:57:10 +0300

    Add new migration file and there models
    Book model and HouseImages models are added and also there migration tables are added

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'price',
+        'title',
+        'author',
+        'description',
+        'language',
+        'paper_back',
+        'image'
+    ];
+
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
