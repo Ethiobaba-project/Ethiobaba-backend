4e2e57a26e9a1e8144f93143521c2bbbf53e4bde
Kuma Telila <kumatelila26@gmail.com>
2023-07-27 21:32:43 +0300

    Admin dashboard formated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,39 +3,12 @@
          <div class="content pt-5 mt-5">
              <div class="container-fluid">
                  <div class="row">
-                     <div class="col-12 col-sm-6 col-md-4">
-                         <div class="info-box">
-                             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-medkit"></i></span>
 
-                             <div class="info-box-content">
-                                 <span class="info-box-text">Cars</span>
-                                 <span class="info-box-number">0</span>
-                             </div>
-                             <!-- /.info-box-content -->
-                         </div>
-                         <!-- /.info-box -->
-                     </div>
-                     <!-- /.col -->
-                     <div class="col-12 col-sm-6 col-md-4">
-                         <div class="info-box mb-3">
-                             <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-ban"></i></span>
-
-                             <div class="info-box-content">
-                                 <span class="info-box-text">remove cars</span>
-                                 <span class="info-box-number">0</span>
-                             </div>
-                             <!-- /.info-box-content -->
-                         </div>
-                         <!-- /.info-box -->
-                     </div>
-                     <!-- /.col -->
-
-                     <!-- fix for small devices only -->
                      <div class="clearfix hidden-md-up"></div>
 
                      <div class="col-12 col-sm-6 col-md-4">
                          <div class="info-box mb-3">
-                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-heartbeat"></i></span>
+                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-car"></i></span>
 
                              <div class="info-box-content">
                                  <span class="info-box-text">Active cars </span>
@@ -46,45 +19,33 @@
                          <!-- /.info-box -->
                      </div>
                      <!-- /.col -->
-                 </div>
 
-                 <div class="row">
-                     <div class="col-12 col-sm-6 col-md-4">
-                         <div class="info-box">
-                             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-medkit"></i></span>
 
-                             <div class="info-box-content">
-                                 <span class="info-box-text">Houses</span>
-                                 <span class="info-box-number">0 </span>
-                             </div>
-                             <!-- /.info-box-content -->
-                         </div>
-                         <!-- /.info-box -->
-                     </div>
-                     <!-- /.col -->
-                     <div class="col-12 col-sm-6 col-md-4">
-                         <div class="info-box mb-3">
-                             <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-ban"></i></span>
-
-                             <div class="info-box-content">
-                                 <span class="info-box-text">remove house</span>
-                                 <span class="info-box-number">0</span>
-                             </div>
-                             <!-- /.info-box-content -->
-                         </div>
-                         <!-- /.info-box -->
-                     </div>
-                     <!-- /.col -->
 
                      <!-- fix for small devices only -->
                      <div class="clearfix hidden-md-up"></div>
 
                      <div class="col-12 col-sm-6 col-md-4">
                          <div class="info-box mb-3">
-                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-heartbeat"></i></span>
+                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-home"></i></span>
 
                              <div class="info-box-content">
                                  <span class="info-box-text">Active house </span>
+                                 <span class="info-box-number">2</span>
+                             </div>
+                             <!-- /.info-box-content -->
+                         </div>
+                         <!-- /.info-box -->
+                     </div>
+                     <!-- fix for small devices only -->
+                     <div class="clearfix hidden-md-up"></div>
+
+                     <div class="col-12 col-sm-6 col-md-4">
+                         <div class="info-box mb-3">
+                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-book"></i></span>
+
+                             <div class="info-box-content">
+                                 <span class="info-box-text">Active Books </span>
                                  <span class="info-box-number">2</span>
                              </div>
                              <!-- /.info-box-content -->
